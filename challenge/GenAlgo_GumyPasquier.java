053a58f9ba9375bfd801738f240d56563862e0bb
Vincent Pasquier <vincent.pasquier@gmail.com>
2011-10-11 16:06:06 +0200

    Création GenAlgo_GumyPasquier qui implement GenAlgo … +formatage

---
+++ challenge/GenAlgo_GumyPasquier.java
@@ -0,0 +1,21 @@
+package challenge;
+
+public class GenAlgo_GumyPasquier implements GenAlgo {
+
+	private static final String GROUP_NAME = "Gumy & Pasquier";
+
+	public GenAlgo_GumyPasquier() {
+
+	}
+
+	@Override
+	public boolean decision(boolean[][] history) {
+		return false;
+	}
+
+	@Override
+	public String getGroupName() {
+		return GROUP_NAME;
+	}
+
+}
